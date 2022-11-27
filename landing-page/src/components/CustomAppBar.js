42d0d01cb09afd867dcf8c02bc3dfde6b35b4fdf
Elias <eliasnemr@gmail.com>
2022-11-27 14:23:20 +0200

    tweaks

--- landing-page/src/components/CustomAppBar.js
+++ landing-page/src/components/CustomAppBar.js
@@ -1,53 +1,78 @@
-import { Toolbar, Box, Grid, Typography, Avatar, Link } from '@mui/material'
-import { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router'
+import { Toolbar, Box, Grid, Typography, Avatar, Link } from "@mui/material";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+
+import styles from "./CustomAppBar.module.css";
 
 export default function CustomAppBar() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
-    setIsLoggedIn(localStorage.getItem('login') === 'true')
-  }, [isLoggedIn])
+    setIsLoggedIn(localStorage.getItem("login") === "true");
+  }, [isLoggedIn]);
 
   return (
     <Box>
       {/* opacity: 0.8 on :hover */}
-      <Toolbar sx={{ color: 'black', cursor: 'pointer' }}>
-        <Grid container alignItems='center'>
-          <Grid item xs={2} sx={{':hover': {opacity: 0.4}}} textAlign='right' onClick={() => navigate('/')}>
-            <Box> <Typography> Home </Typography> </Box>
+      <Toolbar sx={{ color: "black", cursor: "pointer" }}>
+        <Grid container alignItems="center">
+          <Grid item xs={2} textAlign="right" onClick={() => navigate("/")}>
+            <Box className={styles["home__btn__wrapper"]}>
+              {" "}
+              <Typography variant="body1"> Home </Typography>{" "}
+            </Box>
           </Grid>
-          <Grid item xs={2} sx={{':hover': {opacity: 0.4}}} textAlign='right'>
-            <Typography component={Link} color="textPrimary" underline="none"
-              href='https://ossh-messenger.netlify.app/'
-            > Chat 
-            </Typography>
+          <Grid item xs={2} textAlign="right">
+            <Box className={styles["home__btn__wrapper"]}>
+              <Typography
+                component={Link}
+                color="textPrimary"
+                underline="none"
+                href="https://ossh-messenger.netlify.app/"
+              >
+                Chat
+              </Typography>
+            </Box>
           </Grid>
-          <Grid item xs={4} textAlign='center' onClick={() => navigate('/')}>
-            <Box component='img' width='120px' height='auto' src='/assets/logo.png' alt='logo' />
+          <Grid item xs={4} textAlign="center" onClick={() => navigate("/")}>
+            <Box
+              component="img"
+              width="120px"
+              height="auto"
+              src="/assets/logo.png"
+              alt="logo"
+            />
           </Grid>
-          <Grid item xs={2} textAlign='left'>
-            <Typography>  </Typography>
+          <Grid item xs={2} textAlign="left">
+            <Typography> </Typography>
           </Grid>
-          <Grid item xs={2} textAlign='left' onClick={() => {
-            if (!isLoggedIn) navigate('/signup')
-          }}>
-            {isLoggedIn
-              ? <Grid container>
-                  <Grid item xs={6}>
-                    <Typography> Alexandros Yiangou </Typography>
-                  </Grid>
-                  <Grid item xs={6}>
-                    <Avatar />
-                  </Grid>
+          <Grid
+            item
+            xs={2}
+            textAlign="left"
+            onClick={() => {
+              if (!isLoggedIn) navigate("/signup");
+            }}
+          >
+            {isLoggedIn ? (
+              <Grid container>
+                <Grid item xs={6}>
+                  <Typography> Alexandros Yiangou </Typography>
                 </Grid>
-              : <Typography sx={{':hover': {opacity: 0.4}}}> Signup / Login </Typography>
-            }
+                <Grid item xs={6}>
+                  <Avatar />
+                </Grid>
+              </Grid>
+            ) : (
+              <Box className={styles["home__btn__wrapper"]}>
+                <Typography variant="body1"> Signup / Login </Typography>
+              </Box>
+            )}
           </Grid>
         </Grid>
       </Toolbar>
     </Box>
-  )
+  );
 }
--- landing-page/src/pages/Homepage.js
+++ landing-page/src/pages/Homepage.js
@@ -1,116 +1,182 @@
 import {
-  Box, Grid, TableRow,
-  TableCell, Button, TextField, FormControl,
-  InputLabel, Select, MenuItem, Table, TableContainer, TableHead, TableBody
-} from '@mui/material'
-import {map, find, filter, split, has, isEmpty} from 'lodash'
-import { useState, useEffect } from 'react'
-import { useNavigate, useLocation } from 'react-router'
-import CustomAppBar from '../components/CustomAppBar'
+  Box,
+  Grid,
+  TableRow,
+  TableCell,
+  Button,
+  TextField,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  Table,
+  TableContainer,
+  TableHead,
+  TableBody,
+} from "@mui/material";
+import { map, find, filter, split, has, isEmpty } from "lodash";
+import { useState, useEffect } from "react";
+import { useNavigate, useLocation } from "react-router";
+import CustomAppBar from "../components/CustomAppBar";
 
-const locationsList = ['All', 'Famagusta', 'Larnaca', 'Limassol', 'Nicosia', 'Paphos']
-const servicesList = ['All', 'GP', 'Specialist', 'Laboratory', 'Pharmacy']
+const locationsList = [
+  "All",
+  "Famagusta",
+  "Larnaca",
+  "Limassol",
+  "Nicosia",
+  "Paphos",
+];
+const servicesList = ["All", "GP", "Specialist", "Laboratory", "Pharmacy"];
 const specialitiesList = {
-  GP: {key: 'GP', list: ['All', 'GP - Adults', 'GP - Kids']},
-  Specialist: {key: 'Specialist', list: ['All', 'Cardiologist', 'Orthopaedic', '...']},
-  Laboratory: {key: 'Laboratory', list: ['All', 'Biochemistry', 'Hematology', 'Microbiology', '...']}
-}
+  GP: { key: "GP", list: ["All", "GP - Adults", "GP - Kids"] },
+  Specialist: {
+    key: "Specialist",
+    list: ["All", "Cardiologist", "Orthopaedic", "..."],
+  },
+  Laboratory: {
+    key: "Laboratory",
+    list: ["All", "Biochemistry", "Hematology", "Microbiology", "..."],
+  },
+};
 
 const doctorsList = [
-  {name: 'Alexandros', surname: 'Alexandrou', location: 'Limassol', speciality: 'Cardiologist'},
-  {name: 'Andreas', surname: 'Andreou', location: 'Nicosia', speciality: 'Orthopaedic'},
-  {name: 'Charalambos', surname: 'Charalambou', location: 'Paphos', speciality: 'Cardiologist'},
-  {name: 'Elias', surname: 'Elia', location: 'Nicosia', speciality: 'Cardiologist'},
-]
+  {
+    name: "Alexandros",
+    surname: "Alexandrou",
+    location: "Limassol",
+    speciality: "Cardiologist",
+  },
+  {
+    name: "Andreas",
+    surname: "Andreou",
+    location: "Nicosia",
+    speciality: "Orthopaedic",
+  },
+  {
+    name: "Charalambos",
+    surname: "Charalambou",
+    location: "Paphos",
+    speciality: "Cardiologist",
+  },
+  {
+    name: "Elias",
+    surname: "Elia",
+    location: "Nicosia",
+    speciality: "Cardiologist",
+  },
+];
 
 export default function Homepage() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-  const [nameValue, setNameValue] = useState('')
-  const [surnameValue, setSurnameValue] = useState('')
-  const [locationValue, setLocationValue] = useState('All')
-  const [serviceValue, setServiceValue] = useState('All')
-  const [specialityValue, setSpecialityValue] = useState('')
-  const [searchResult, setSearchResult] = useState([])
-  const hasSpecialityList = Boolean(find(specialitiesList, {key: serviceValue}))
+  const [nameValue, setNameValue] = useState("");
+  const [surnameValue, setSurnameValue] = useState("");
+  const [locationValue, setLocationValue] = useState("All");
+  const [serviceValue, setServiceValue] = useState("All");
+  const [specialityValue, setSpecialityValue] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+  const hasSpecialityList = Boolean(
+    find(specialitiesList, { key: serviceValue })
+  );
 
   const doSearch = () => {
-    let filteredList = doctorsList
+    let filteredList = doctorsList;
 
-    if (nameValue !== '') filteredList = filter(filteredList, {name: nameValue})
-    if (surnameValue !== '') filteredList = filter(filteredList, {surname: surnameValue})
-    if (locationValue !== 'All') filteredList = filter(filteredList, {location: locationValue})
-    if (specialityValue !== '') filteredList = filter(filteredList, {speciality: specialityValue})
+    if (nameValue !== "")
+      filteredList = filter(filteredList, { name: nameValue });
+    if (surnameValue !== "")
+      filteredList = filter(filteredList, { surname: surnameValue });
+    if (locationValue !== "All")
+      filteredList = filter(filteredList, { location: locationValue });
+    if (specialityValue !== "")
+      filteredList = filter(filteredList, { speciality: specialityValue });
 
-    setSearchResult(filteredList)
-  }
+    setSearchResult(filteredList);
+  };
 
-  const location = useLocation()
+  const location = useLocation();
 
   useEffect(() => {
-    const params = {}
-    const query = location.search.substring(1)
-    const pairs = query ? split(query, '&') : ''
+    const params = {};
+    const query = location.search.substring(1);
+    const pairs = query ? split(query, "&") : "";
 
     for (let i = 0; i < pairs.length; i++) {
-      const pair = split(pairs[i], '=')
-      params[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1] || '');
+      const pair = split(pairs[i], "=");
+      params[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1] || "");
     }
 
-    if ((localStorage.getItem('login') === 'true') && (has(params, 'login') && params.login === '1')) {
-      return
-    } else if(isEmpty(params)) {
-      return
+    if (
+      localStorage.getItem("login") === "true" &&
+      has(params, "login") &&
+      params.login === "1"
+    ) {
+      return;
+    } else if (isEmpty(params)) {
+      return;
     } else {
-      localStorage.setItem('login', params.login === '1')
+      localStorage.setItem("login", params.login === "1");
     }
-  }, [location.search])
-  
+  }, [location.search]);
+
   return (
     <Box>
       <CustomAppBar />
-      <Grid container align='center'>
+      <Grid container align="center">
         <Grid item xs={12}>
-        <Box
-          component='img'
-          sx={{
-            width: 0.9,
-            height: '500px',
-          }}
-          src='/assets/home2.jpg'
-          alt='image'
-        />
+          <Box
+            component="img"
+            sx={{
+              width: "100%",
+              height: "500px",
+            }}
+            src="/assets/home2.jpg"
+            alt="image"
+          />
         </Grid>
       </Grid>
-      <Grid container direction='column' sx={{ padding: '32px' }} rowSpacing={2}>
+      <Grid
+        container
+        direction="column"
+        sx={{ padding: "32px" }}
+        rowSpacing={2}
+      >
         <Grid item xs={12}>
           <Grid container columnSpacing={4}>
             <Grid item xs={4}>
-              <TextField fullWidth sx={{ backgroundColor: 'white' }}
-                label='Name'
+              <TextField
+                fullWidth
+                sx={{ backgroundColor: "white" }}
+                label="Name"
                 onChange={(event) => setNameValue(event.target.value)}
               />
             </Grid>
             <Grid item xs={4}>
-              <TextField fullWidth sx={{ backgroundColor: 'white' }}
-                label='Surame'
+              <TextField
+                fullWidth
+                sx={{ backgroundColor: "white" }}
+                label="Surame"
                 onChange={(event) => setSurnameValue(event.target.value)}
               />
             </Grid>
             <Grid item xs={4}>
-              <FormControl fullWidth sx={{ backgroundColor: 'white' }}>
+              <FormControl fullWidth sx={{ backgroundColor: "white" }}>
                 <InputLabel id="location-select-label"> Location </InputLabel>
                 <Select
                   labelId="location-select-label"
                   value={locationValue}
                   label="Location"
                   onChange={(event) => {
-                    setLocationValue(event.target.value)
+                    setLocationValue(event.target.value);
                     // setTobaccoBrandError('')
                   }}
                 >
                   {locationsList.map((option) => (
-                    <MenuItem key={option} value={option}> {option} </MenuItem>
+                    <MenuItem key={option} value={option}>
+                      {" "}
+                      {option}{" "}
+                    </MenuItem>
                   ))}
                 </Select>
               </FormControl>
@@ -120,40 +186,51 @@ export default function Homepage() {
         <Grid item xs={12}>
           <Grid container columnSpacing={4}>
             <Grid item xs={6}>
-              <FormControl fullWidth sx={{ backgroundColor: 'white'}}>
+              <FormControl fullWidth sx={{ backgroundColor: "white" }}>
                 <InputLabel id="category-select-label"> Service </InputLabel>
                 <Select
                   labelId="category-select-label"
                   value={serviceValue}
                   label="Service"
                   onChange={(event) => {
-                    setServiceValue(event.target.value)
+                    setServiceValue(event.target.value);
                   }}
                 >
                   {servicesList.map((option) => (
-                    <MenuItem key={option} value={option}> {option} </MenuItem>
+                    <MenuItem key={option} value={option}>
+                      {" "}
+                      {option}{" "}
+                    </MenuItem>
                   ))}
                 </Select>
               </FormControl>
             </Grid>
             <Grid item xs={6}>
-              <FormControl fullWidth sx={{ backgroundColor: 'white' }}
-              disabled={!hasSpecialityList}>
-                <InputLabel id="speciality-select-label"> Speciality </InputLabel>
+              <FormControl
+                fullWidth
+                sx={{ backgroundColor: "white" }}
+                disabled={!hasSpecialityList}
+              >
+                <InputLabel id="speciality-select-label">
+                  {" "}
+                  Speciality{" "}
+                </InputLabel>
                 <Select
                   labelId="speciality-select-label"
                   value={specialityValue}
                   label="Speciality"
                   onChange={(event) => {
-                    setSpecialityValue(event.target.value)
+                    setSpecialityValue(event.target.value);
                   }}
                 >
-                  {find(specialitiesList, {key: serviceValue}) 
+                  {find(specialitiesList, { key: serviceValue })
                     ? map(specialitiesList[serviceValue].list, (option) => (
-                      <MenuItem key={option} value={option}> {option} </MenuItem>
-                    ))
-                    : null
-                  }
+                        <MenuItem key={option} value={option}>
+                          {" "}
+                          {option}{" "}
+                        </MenuItem>
+                      ))
+                    : null}
                 </Select>
               </FormControl>
             </Grid>
@@ -162,10 +239,25 @@ export default function Homepage() {
         <Grid item xs={12}>
           <Grid container columnSpacing={4}>
             <Grid item xs={6}>
-              <Button fullWidth variant='contained' sx={{backgroundColor: '#153D72'}}> Clear </Button>
+              <Button
+                fullWidth
+                variant="contained"
+                sx={{ backgroundColor: "#153D72" }}
+              >
+                {" "}
+                Clear{" "}
+              </Button>
             </Grid>
             <Grid item xs={6}>
-              <Button fullWidth variant='contained' sx={{backgroundColor: '#153D72'}} onClick={() => doSearch()}> Search </Button>
+              <Button
+                fullWidth
+                variant="contained"
+                sx={{ backgroundColor: "#153D72" }}
+                onClick={() => doSearch()}
+              >
+                {" "}
+                Search{" "}
+              </Button>
             </Grid>
           </Grid>
         </Grid>
@@ -181,8 +273,8 @@ export default function Homepage() {
           </TableHead>
           <TableBody>
             {searchResult.map((option) => (
-              <TableRow onClick={() => navigate('/doctor')}>
-                <TableCell> {option.name + ' ' + option.surname} </TableCell>
+              <TableRow onClick={() => navigate("/doctor")}>
+                <TableCell> {option.name + " " + option.surname} </TableCell>
                 <TableCell> {option.location} </TableCell>
                 <TableCell> {option.speciality} </TableCell>
               </TableRow>
@@ -201,5 +293,5 @@ export default function Homepage() {
         </Table>
       </TableContainer>
     </Box>
-  )
+  );
 }
